c9b802a2226b69b37cfa9b6cc09028db9bceacc4
Thinaya <81453008+Thinaya@users.noreply.github.com>
2022-02-19 14:32:27 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+    }
+    
+    function preload() {
+    }
+    
+    function draw() {
+    image(video, 0, 0, 480, 380);
+    }
+    
+    function start() {
+        objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+        document.getElementById('status').innerHTML = "Status - Detecting Objects";
+    }
+    
+    function modelLoaded() {
+        console.log('Model Loaded');
+        status=true;
+    }
